9a4a1a4cda3f41d694dd3dd4630d844033739ef5
modeste <gougbedjimodeste@gmail.com>
2017-01-23 05:20:46 -1100

    Error in JS code

--- admin/js/botamp-admin.js
+++ admin/js/botamp-admin.js
@@ -1,38 +1,38 @@
 (function( $ ) {
 	'use strict';
-	$(document).ready(function () {
+	$( document ).ready( function () {
 		function get_post_type_fields() {
-			$('.post-type-validate').change(function () {
-				if ($('.post-type-validate:checked').length) {
+			$( '.post-type-validate' ).change( function () {
+				if ( $( '.post-type-validate:checked' ).length ) {
 					var is_post_type_verified = false;
-					$(".botamp-post-type").each(function() {
-						var current_post_type = $(".botamp-get-list-post-type").val();
-						if (current_post_type.search(this.value) === -1) {
+					$( ".botamp-post-type" ).each( function() {
+						var current_post_type = $( ".botamp-get-list-post-type" ).val();
+						if ( current_post_type.search( this.value ) === -1 ) {
 							current_post_type += this.value + ",";
-						}else{
+						} else {
 							is_post_type_verified = true;
-							alert("This Post Type is already checked");
+							alert( "This Post Type is already checked" );
 						}
-						$(".botamp-get-list-post-type").val(current_post_type);
+						$( ".botamp-get-list-post-type" ).val( current_post_type );
 			    	});
-			    	$('.botamp-all-fields').each(function() {
-				       	var select_parent = $(this).parent();
-				       	var current_field = select_parent.find(".botamp-get-list-fields").val();
+			    	$( '.botamp-all-fields' ).each( function() {
+				       	var select_parent = $( this ).parent();
+				       	var current_field = select_parent.find( ".botamp-get-list-fields" ).val();
 				       	if (is_post_type_verified === false) {
-				       		if (!this.value) {
+				       		if ( !this.value ) {
 								current_field += "post_thumbnail_url,";
-							}else{
+							} else {
 								current_field += this.value + ",";
 							}
 				       	}
-				       	select_parent.find(".botamp-get-list-fields").val(current_field);
-				       	$(".botamp-show-user-fields").text($(".botamp-get-list-fields").val());
-				    });  
+				       	select_parent.find( ".botamp-get-list-fields" ).val( current_field );
+				       	$( ".botamp-show-user-fields" ).text( $( ".botamp-get-list-fields" ).val() );
+				    } );
 				}
 			}).change();
 		}
 		$(".botamp-post-type").change(function () {
-		    $(".botamp-display-checkbox").html('</br><input class="post-type-validate" type="checkbox"/>');  
+		    $( ".botamp-display-checkbox" ).html( '</br><input class="post-type-validate" type="checkbox"/>' );  
 			get_post_type_fields();
 		}).change();
 	});
