0dedf6beaaa7db8948b874092e276024ee4a9e21
modeste <gougbedjimodeste@gmail.com>
2017-02-04 03:56:57 -1100

    import post that corresponds to post type

--- admin/partials/botamp-admin-display-import.php
+++ admin/partials/botamp-admin-display-import.php
@@ -1,17 +1,28 @@
 <?php
-	global $wpdb;
-	$posts = $wpdb->get_col( $wpdb->prepare(
-		"SELECT $wpdb->posts.ID
-        FROM $wpdb->posts
-        WHERE $wpdb->posts.post_type = %s
-        AND $wpdb->posts.post_status = 'publish'
-        AND $wpdb->posts.ID
-        NOT IN (
-            SELECT $wpdb->postmeta.post_id
-            FROM $wpdb->postmeta
-            WHERE  $wpdb->postmeta.meta_key = 'entity_id'
-        )", $this->get_option( 'post_type' ) ) );
-
+global $wpdb;
+$all_posts_valid = [];
+$posts = [];
+foreach ( $this->get_option( 'post_type' ) as $post_type => $entity_fields ) {
+	if ( $entity_fields['valid'] === 'enabled' ) {
+				array_push( $all_posts_valid, $wpdb->get_col( $wpdb->prepare(
+							"SELECT $wpdb->posts.ID
+									FROM $wpdb->posts
+									WHERE $wpdb->posts.post_type = %s
+									AND $wpdb->posts.post_status = 'publish'
+									AND $wpdb->posts.ID
+									NOT IN (
+											SELECT $wpdb->postmeta.post_id
+											FROM $wpdb->postmeta
+											WHERE  $wpdb->postmeta.meta_key = 'entity_id'
+									)", $post_type ) )
+				);
+	}
+}
+foreach( $all_posts_valid as $key => $content ) {
+	for( $i=0; $i<count($content); $i++ ) {
+		array_push( $posts, $content[$i] );
+	}
+}
 ?>
 <h3 class="title"> <?php _e( 'Importing all existing posts into your Botamp app', 'botamp' ) ?> </h3><hr>
 <p>
